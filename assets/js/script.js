9b590c82e5214cdd5eee2e75546123ab3953c290
Josue Enriquez <elcolornoloestodo@gmail.com>
2022-02-26 04:13:52 -0500

    Variables de elementos HTML agregadas a script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
 'use strict'
+
+//! HTML Elements
+
+const bntReplay = document.getElementById("buttonReplay");
+const board = document.getElementById("board");
+const score = document.getElementById("scoreIndicator");
